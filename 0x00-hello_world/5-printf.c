134b5fa05205cf9b8c9887fde76d56812e9e6fb6
Phecobaba <mrpheco@gmail.com>
2023-02-17 08:08:38 -0800

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stderr>
 
 int main(void)
 {
-	printf("%s \n", "with proper grammar, but the outcome is a piece of art");
+	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }	
