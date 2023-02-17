d5eab489f626bba06971dfd5d3572df675cd732a
Phecobaba <mrpheco@gmail.com>
2023-02-17 10:31:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+/**
+ * main- prints out the paranthesized text
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're free to use several lines to properly describe your function
+ * Return: The paranthesized characters
+ */
 
 int main(void)
 {
-	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	printf("%s\n", "with proper grammar,but the outcome is a piece of art,");
 
-	return (0);
-}	
-
+return (0); }
