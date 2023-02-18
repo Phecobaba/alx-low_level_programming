5590fc0df255f15ee18efbe364a53402f62ae0ac
Phecobaba <mrpheco@gmail.com>
2023-02-18 13:52:07 -0800

    finished

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
-{ #include <stdio.h>
+#include <stdio.h>
 /**
  * Main- it describes the string
  * Return- it returns the value 1
+ */
 
 int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-
 return (1);
-} }
+}
