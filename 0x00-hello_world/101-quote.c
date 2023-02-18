b0d2b9f6b71712ea428aa5fa53bfb3e1556d7aff
Phecobaba <mrpheco@gmail.com>
2023-02-18 13:57:18 -0800

    file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,8 @@
  * Return- it returns the value 1
  */
 
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+int main(void) 
+{	
+fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
