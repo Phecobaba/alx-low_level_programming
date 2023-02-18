d6387c17cf5264779a7ad0b2757babcf247bc030
Phecobaba <mrpheco@gmail.com>
2023-02-18 03:11:32 -0800

    pheco

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 
 int main() {
-  printf("Size of a char : %2d bytes\n", sizeof(char));
-  printf("Size of an Int :  %2d bytes\n", sizeof(int));
-  printf("Size of a long int : %2d bytes\n", sizeof(long int));
-  printf("Size of Long Long int : %2d bytes\n", sizeof(long long int));
-  printf("Size of a float : %2d bytes\n", sizeof(float));
+  printf("Size of a char : %2dbytes\n", sizeof(char));
+  printf("Size of an Int :  %2dbytes\n", sizeof(int));
+  printf("Size of a long int : %2dbytes\n", sizeof(long int));
+  printf("Size of Long Long int : %2dbytes\n", sizeof(long long int));
+  printf("Size of a float : %2dbytes\n", sizeof(float));
   return 0;
 }
