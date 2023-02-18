32179e2fbb5e032538f567bf23f7d9746afb3727
Phecobaba <mrpheco@gmail.com>
2023-02-18 02:49:58 -0800

    push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 
 int main() {
-  printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
-  printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
-  printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
-  printf("Size of Long Long Data Types in C = %2d bytes \n", sizeof(long double));
-  printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+  printf("Size of a char Data Types in C = %2d bytes \n", sizeof(char));
+  printf("Size of an Int Data Types in C = %2d bytes \n", sizeof(short int));
+  printf("Size of a long int Data Types in C = %2d bytes \n", sizeof(long int));
+  printf("Size of Long Long int Data Types in C = %2d bytes \n", sizeof(long double));
+  printf("Size of a float Data Types in C = %2d bytes \n", sizeof(float));
   return 0;
 }
