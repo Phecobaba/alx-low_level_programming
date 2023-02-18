0a4fd991203708b3aeb08bf605b026c65495d412
Phecobaba <mrpheco@gmail.com>
2023-02-18 12:57:04 -0800

    fight

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *Return: 0 is exited properly
  */
 
-int main() {
+int main(void) {
   printf("Size of a char: %2d bytes \n", sizeof(char));
   printf("Size of an int: %2d bytes \n", sizeof(int));
   printf("Size of a long int: %2d bytes \n", sizeof(long int));
