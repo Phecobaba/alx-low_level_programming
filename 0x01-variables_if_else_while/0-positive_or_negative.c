661dba997f566721b9df91a3334a9500b8dac605
Phecobaba <mrpheco@gmail.com>
2023-02-19 10:10:01 -0800

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/** Main- calculates random number for positive, zero, and negative
+ * Return- Returns paranthesized value
+ */
+ 
 int main(void)
 {
 	int n;
