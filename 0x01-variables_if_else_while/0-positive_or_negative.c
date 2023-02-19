96fc45b43b22ccf82c48248aa1a4cfbf8cbb0f88
Phecobaba <mrpheco@gmail.com>
2023-02-19 09:25:25 -0800

    push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if( n > 0 ){
-		printf(rand() "is positive \n");
+		printf("n is positive \n" );
 }
 
 	if( n == 0 ){
-		printf(rand() " is zero \n");
+		printf("n is zero \n");
 }
 	if( n < 0 ) {
-		printf(rand() " is negative \n");
+		printf("n is negative \n");
 }			
 	return (0);
 }
