a69e671be4509cc4b8a01cd0fdcb414d62d3828e
Phecobaba <mrpheco@gmail.com>
2023-02-20 04:51:32 -0800

    game on

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,17 @@ i
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 {
-	printf("last digit of %d is %d > 5
-		else if (n == 0)
-		printf("and is 0")
-		else if (n < 6; != 0)
-		printf("and is less than 6 and not 0")	
+	if (n > 5);
+{
+	printf("last digit of %d is %d and is greater than 5")
+}	
+	if (n == 0);
+{
+	printf("last digit of %d is %d and is 0")
+}		
 
+	if (n < 6);
+{
+	printf("last digit of %d is %d and is less than 6 and not 0")
+}		
 	return (0);
