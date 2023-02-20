a102a18c7bce1aa4d4b037ecf5c86ed3bac5f54d
Phecobaba <mrpheco@gmail.com>
2023-02-20 14:36:18 -0800

    reversed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char j;
 
-	for (j = 'A'; j <= 'Z'; ++j)
+	for (j = 'a'; j <= 'z'; ++j)
 	putchar(j);
 
 	/* for small letters */
-	for (j = 'a'; j <= 'z'; ++j)
+	for (j = 'A'; j <= 'Z'; ++j)
 	putchar(j);
 	putchar('\n');
 	return (0);
