0e30c4df6a15e195668dbc6a3c338d01796e246b
Phecobaba <mrpheco@gmail.com>
2023-02-20 03:49:52 -0800

    file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+for (ch= ‘a’; ch<= ‘z’; ch++)
 
-for(int x = 'A'; x <= 'Z'; x++)
-{
-    int lower_x = tolower(x);
-    putchar(lower_x);
-}
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+                  getch();
