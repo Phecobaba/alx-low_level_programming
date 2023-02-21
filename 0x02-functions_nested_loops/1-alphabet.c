8c1ed7e5640183983efbb6b61e337b7914e19e86
Phecobaba <mrpheco@gmail.com>
2023-02-21 11:21:04 -0800

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,14 +7,14 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-{
+
 	_putchar('i');
 	_putchar('\n');
-}
-	return (0);
+
+
 }
