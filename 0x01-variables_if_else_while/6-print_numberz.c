c2dfb189627424fb46d1e047e0c67facfb3054a4
Phecobaba <mrpheco@gmail.com>
2023-02-20 16:55:05 -0800

    file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 9; p++)
+
+	putchar(p);
+
+	putchar('\n');
+
+	return (0);
+}
