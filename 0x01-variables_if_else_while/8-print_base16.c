ff217bcb9f630b08ddcf65745c5dd3448f583c27
Phecobaba <mrpheco@gmail.com>
2023-02-20 17:28:38 -0800

    why

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int u;
+	char bar;
+
+	for (u = '0'; u <= '9'; u++)
+
+	putchar(u);
+
+	for (bar = 'a'; bar <= 'f'; bar++)
+
+	putchar(bar);
+
+	putchar('\n');
+
+	return (0);
+}
