534061ed64d9662aa27316afcae210ca05fcda80
Phecobaba <mrpheco@gmail.com>
2023-02-25 15:02:18 -0800

    how

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
+void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
 int _isupper(int c);
