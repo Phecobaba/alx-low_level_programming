c5d1cb9407e4cb3cb2438d17454e48f06a6945ca
Phecobaba <mrpheco@gmail.com>
2023-03-20 16:24:55 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,44 @@
+#ifndef INIT_DOG_C
+#define INIT_DOG_C
+
+#define STRUCT_DOG
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *my_dog
+
+	user = malloc (sizeof(struct dog));
+		if (dog == null)
+			return (1);
+
+		dog->name = name;
+		dog->age = age;
+		dog->owner = owner;
+
+	return (dog);
+}
+
+
+
+
+
+
+
+
+
+
+#endif
