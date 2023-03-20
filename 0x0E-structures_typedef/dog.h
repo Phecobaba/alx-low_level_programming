38a6566f7717f285ec6cbab42b5c7f17ff35df96
Phecobaba <mrpheco@gmail.com>
2023-03-20 16:00:13 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+#define STRUCT_DOG
+
+/**
+ *struct dog - defines the type
+ *@name: prints dog's name
+ *@owner: prints dog's owner
+ *@age: prints dog's age
+ *DESCRIPTION: describes the the return type
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
