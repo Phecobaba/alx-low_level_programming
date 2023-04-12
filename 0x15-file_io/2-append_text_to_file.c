b21f51f3d8450581414d93cd6b37e83a63e10a29
Phecobaba <mrpheco@gmail.com>
2023-04-12 15:40:46 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include "main.h"
-#include <sys/stat.h>
-#include <fcntl.h>
+
 /**
- *
- *
  *
  *
  *
@@ -17,25 +11,28 @@
  */
 
 int append_text_to_file(const char *filename, char *text_content) {
-    int fd;
+    FILE *fp;
+    int result;
 
     if (filename == NULL) {
         return -1;
     }
 
-    fd = fopen(filename, O_WRONLY | O_APPEND);
-    if (fd == NULL) {
+    if (text_content == NULL) {
+        return 1;
+    }
+
+    fp = fopen(filename, "a");
+    if (fp == NULL) {
         return -1;
     }
 
-    if (text_content != NULL) {
-        fprintf(fd, " %s", text_content);
-        if (ferror(fd)) {
-            fclose(fd);
-            return -1;
-        }
+    result = fprintf(fp, "%s", text_content);
+    fclose(fp);
+
+    if (result < 0) {
+        return -1;
     }
 
-    fclose(fd);
     return 1;
 }
