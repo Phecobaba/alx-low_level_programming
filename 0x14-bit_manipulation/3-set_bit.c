f8ef4bc0f303dbde36f45bebd67fdf8d4d6b3055
Phecobaba <mrpheco@gmail.com>
2023-05-05 16:29:25 -0700

    set

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * set_bit - set the value
+ * @n: pointer to integer
+ * @index: second parameter
+ * Return: return 1 if succesful
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n |= (1UL << index);
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+	return (-1);
+	}
+	return (1);
+}
