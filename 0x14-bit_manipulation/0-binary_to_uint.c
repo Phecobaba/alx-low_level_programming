009ab180e70eebc79f9d835a2582eeec9a3c1d48
Phecobaba <mrpheco@gmail.com>
2023-05-25 07:13:40 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,29 +12,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int len = strlen(b);
-	int i;
+	unsigned int i = 0, pow = 1, rem = 0;
+
 
 	if (b == NULL)
-	return (0);
-
-	for (i = 0; i < len; i++)
-	{
-	if (b[i] == '0')
-	{
-		result <<= 1;
-	}
-	else if (b[i] == '1')
-	{
-		result <<= 1;
-		result |= 1;
-	}
-	else
-	{
 		return (0);
+	while (b[i])
+		i++;
+	while (i > 0)
+	{
+		i--;
+		if (b[i] == '0' || b[i] == '1')
+		{
+			if (b[i] != '0')
+				rem += pow;
+			pow *= 2;
+		}
+		else
+			return (0);
 	}
-	}
-
-	return (result);
+	return (rem);
 }
