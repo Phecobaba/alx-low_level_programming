feb675c4dfe4a87dace85c28b9da189b96f755ce
Phecobaba <mrpheco@gmail.com>
2023-05-25 07:03:44 -0700

    read text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,51 +3,54 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "main.h"
+#include "main.h"
+
 /**
- *read_textfile - it reads the text
- *@filename: First parameter indicating pointer to filename
- *@letters: Second parameter to count the number of letters
- *Return: returns the total read
- */
+* read_textfile - function that reads a text file and prints it.
+* @filename: file to read from
+* @letters: number of chars to read
+* Return: number of letters read or 0 if it fails
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	size_t num_read;
-	FILE *fp;
-	ssize_t total_read = 0;
-	size_t bytes_written;
+	ssize_t n, w = 0, r_b = 0;
+	int fil;
+	char *st;
 
 	if (filename == NULL)
-	{
-	return (0);
-	}
+		return (0);
+	fil = open(filename, O_RDONLY);
+	if (fil == -1)
+		return (0);
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
+	st = malloc(letters + 1);
+	if (st == NULL)
+		return (0);
+	while (letters > 0)
 	{
-	return (0);
+		n = read(fil, st, letters);
+		if (n == -1)
+		{
+			free(st);
+			close(fil);
+			return (0);
+		}
+		if (n == 0)
+			break;
+		st[n] = '\0';
+		w += write(STDOUT_FILENO, st, n);
+		if (w == -1)
+		{
+			free(st);
+			close(fil);
+			return (r_b);
+		}
+		r_b += n;
+		letters -= n;
+		if (letters == 0)
+			break;
 	}
-
-	buffer = (char *)malloc(letters * sizeof(char));
-	if (buffer == NULL)
-	{
-	fclose(fp);
-	return (0);
-	}
-
-	while ((num_read = fread(buffer, sizeof(char), letters, fp)) > 0)
-	{
-	bytes_written = write(STDOUT_FILENO, buffer, num_read);
-	if (bytes_written != num_read)
-	{
-	fclose(fp);
-	free(buffer);
-	return (0);
-	}
-	total_read += num_read;
-	}
-
-	fclose(fp);
-	free(buffer);
-	return (total_read);
+	free(st);
+	close(fil);
+	return (r_b);
 }
